27d18abaf4ece14f2429084a6e35a106605be1b4
ewwong2 <ewwong2@dons.usfca.edu>
2020-04-19 20:15:16 -0700

    Initial visuaizations completed

--- assets/js/dendrogram.js
+++ assets/js/dendrogram.js
@@ -1,23 +1,64 @@
 ---
 ---
-const width = 954
+const svg = d3.select("body").select("svg#dendrogram");
+
+const width = 960;
+const height = 600;
+const radius = width / 2;
+
+const tree = d3.cluster().size([2 * Math.PI, radius - 100]);
+
+const color = d3.scaleSequential(d3.interpolateYlOrRd);
+
+const tip = d3.select("body")
+  .append("pre")
+  .attr("id", "tooltip")
+  .style("opacity", "0")
+  .style("background-color", "rgba(255, 255, 255, 0.7)")
+  .style("padding", "0px 5px")
+  .style("border-radius", "8px");
+
+var gData;
 
 d3.csv('{{ '/assets/data/dendrogram.csv' | prepend: site.baseurl }}', convert).then(data => {
   console.log(data);
-  let clusterJson = filterData(data);
+  gData = data;
+  draw();
+  svg.attr("viewBox", autoBox);
+  svg.select("g#links").attr("transform", "translate(0,100)");
+  svg.select("g#circles").attr("transform", "translate(0,100)");
+  svg.select("g#labels").attr("transform", "translate(0,100)");
+});
+
+window.onload = function () {
+  document.getElementById("filter").addEventListener("input", function (e) {
+    draw(this.value)
+  });
+}
+
+function draw(time) {
+  let data = gData;
+  let [clusterJson, min, max] = time? filterData(data, time): filterData(data);
+  color.domain([min, max]);
   console.log(clusterJson);
   data = clusterJson;
-  let tree = data => {
-    const root = d3.hierarchy(data).sort((a, b) => d3.descending(a.height, b.height) || d3.ascending(a.data.name, b.data.name));
-    root.dx = 10;
-    root.dy = width / (root.height + 1);
-    return d3.cluster().nodeSize([root.dx, root.dy])(root);
-  }
 
-  const root = tree(data);
-  const svg = d3.select("body").select("svg#dendrogram");
+  let root = tree(d3.hierarchy(data)
+      .sort((a, b) => d3.ascending(a.data.name, b.data.name)));
 
-  svg.append("g")
+  d3.select("g#links").html(null);
+  drawLinks(root);
+  d3.select("g#circles").html(null);
+  let circles = drawCircles(root);
+  circleInteractivity(circles);
+  d3.select("g#labels").html(null);
+  drawLabels(root);
+  d3.select("g#legend").html(null);
+  drawLegend(min, max);
+}
+
+function drawLinks(root) {
+  svg.select("g#links")
     .attr("fill", "none")
     .attr("stroke", "#555")
     .attr("stroke-opacity", 0.4)
@@ -25,23 +66,107 @@ d3.csv('{{ '/assets/data/dendrogram.csv' | prepend: site.baseurl }}', convert).t
   .selectAll("path")
     .data(root.links())
     .join("path")
-      .attr("d", d => `
-        M${d.target.y},${d.target.x}
-        C${d.source.y + root.dy / 2},${d.target.x}
-         ${d.source.y + root.dy / 2},${d.source.x}
-         ${d.source.y},${d.source.x}
-      `);
+      .attr("d", d3.linkRadial()
+          .angle(d => d.x)
+          .radius(d => d.y))
+      .attr("id", d => `${garbageId(d.target)}_link`);
+}
 
-  svg.append("g")
+function circleInteractivity(circles) {
+  circles.on("mouseover", function(d) {
+    let curr = d;
+    while (curr.parent) {
+      let baseId = garbageId(curr);
+      let me = d3.select(`circle#${baseId}`);
+      me.raise();
+      me.transition().attr("stroke", "#f24141")
+        .style("stroke-width", "2px");
+      d3.select(`path#${baseId}_link`).transition()
+        .attr("stroke", "#f24141")
+        .style("stroke-width", "2px");
+      curr = curr.parent;
+    }
+    let me = d3.select(`circle#${garbageId(curr)}`);
+    me.raise();
+    me.transition().attr("stroke", "#f24141")
+      .style("stroke-width", "2px");
+
+    let shadow = d3.select(d.parent? `text#shadow_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "text#shadow_root");
+    let text = d3.select(d.parent? `text#${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "text#root");
+    shadow.raise();
+    text.raise();
+    text.attr("font-weight", "bold");
+    shadow.attr("font-weight", "bold");
+
+    // get height of tooltip
+    let bbox = tip.node().getBoundingClientRect();
+
+    // https://stackoverflow.com/questions/4666367/how-do-i-position-a-div-relative-to-the-mouse-pointer-using-jquery
+    tip.style("left", (d3.event.pageX) + "px")
+    tip.style("top",  (d3.event.pageY - bbox.height) + "px");
+    tip.text(d.children? `${d.data.name}`:`${d.data.name}\n#: ${d3.format("~s")(d.data.value)}`);
+    tip.transition().style("opacity", "1");
+  }).on("mouseout", function(d) {
+    let curr = d;
+    while (curr.parent) {
+      let baseId = garbageId(curr);
+      let me = d3.select(`circle#${baseId}`);
+      me.raise();
+      me.transition()
+        .attr("stroke", curr.children? "black": d3.lab(color(curr.data.value)).darker())
+        .style("stroke-width", "1px");
+      d3.select(`path#${baseId}_link`).transition()
+        .attr("stroke", "black")
+        .style("stroke-width", "1px");
+      curr = curr.parent;
+    }
+    let me = d3.select(`circle#${garbageId(curr)}`);
+    me.raise();
+    me.transition()
+      .attr("stroke", "black")
+      .style("stroke-width", "1px");
+
+    let shadow = d3.select(d.parent? `text#shadow_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "text#shadow_root");
+    let text = d3.select(d.parent? `text#${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "text#root");
+    text.attr("font-weight", "normal");
+    shadow.attr("font-weight", "normal");
+    tip.transition().style("opacity", "0");
+  });
+}
+
+// This should prob be a loop... but whatever
+function garbageId(d) {
+  let id = d.parent?
+    d.parent.parent?
+      d.parent.parent.parent?
+        `${d.parent.parent.parent.data.name}_${d.parent.parent.data.name}_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+        :
+        `${d.parent.parent.data.name}_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+      :
+      `${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+    :
+    `${d.data.name}`.replace(/\s/g,'');
+  return id;
+}
+
+function drawCircles(root) {
+  let circles = svg.select("g#circles")
     .selectAll("circle")
     .data(root.descendants())
     .join("circle")
-      .attr("cx", d => d.y)
-      .attr("cy", d => d.x)
-      .attr("fill", d => d.children ? "#555" : "#999")
-      .attr("r", 2.5);
+      .attr("transform", d => `
+        rotate(${d.x * 180 / Math.PI - 90})
+        translate(${d.y},0)
+      `)
+      .attr("fill", d => d.children? "#999": color(d.data.value))
+      .attr("stroke", d => d.children? "black": d3.lab(color(d.data.value)).darker())
+      .attr("r", 5)
+      .attr("id", d => garbageId(d));
+  return circles;
+}
 
-  svg.append("g")
+function drawLabels(root) {
+  svg.select("g#labels")
       .attr("font-family", "sans-serif")
       .attr("font-size", 10)
       .attr("stroke-linejoin", "round")
@@ -49,18 +174,76 @@ d3.csv('{{ '/assets/data/dendrogram.csv' | prepend: site.baseurl }}', convert).t
     .selectAll("text")
     .data(root.descendants())
     .join("text")
-      .attr("x", d => d.y)
-      .attr("y", d => d.x)
-      .attr("dy", "0.31em")
-      .attr("dx", d => d.children ? -6 : 6)
-      .text(d => d.data.name)
-    .filter(d => d.children)
-      .attr("text-anchor", "end")
-    .clone(true).lower()
-      .attr("stroke", "white");
+    .attr("transform", d => `
+      rotate(${d.x * 180 / Math.PI - 90})
+      translate(${d.y},0)
+      rotate(${d.x >= Math.PI ? 180 : 0})
+    `)
+    .attr("id", d => d.parent? `${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "root")
+    .attr("dy", "0.31em")
+    .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+    .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+    .text(d => d.data.name)
+  .clone(true).lower()
+    .attr("stroke", "white")
+    .attr("id", d => d.parent? `shadow_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,''): "shadow_root");
+}
 
-  // svg.attr("viewBox", autoBox);
-});
+function drawLegend(min, max) {
+  let width = radius + 70;
+  let config = {
+    "margin": {
+      "left": 50,
+      "right": 50,
+      "top": -100,
+      "bot": 50
+    },
+    "height": height - 300,
+    "width": 15
+  }
+  // Sequential Legend created from following Tom MacWright's example
+  // here: https://observablehq.com/@tmcw/d3-scalesequential-continuous-color-legend-example
+  svg.select("g#legend").append("text")
+    .attr("x", width - config.margin.right - 10)
+    .attr("y", 2*config.margin.top - 5)
+    .text("#");
+
+  let legendScale = d3.scaleLinear()
+    .domain(color.domain().reverse())
+    .range([config.margin.top, config.height])
+
+  let legendAxis = g => g
+    .attr("class", `legend-axis`)
+    .attr("transform", `translate(${width - config.margin.right}, ${config.margin.top})`)
+    .attr("height", config.height)
+    .call(d3.axisRight(legendScale)
+      .tickFormat(d3.format("~s"))
+      .ticks(config.height / 40)
+      .tickSize(-config.width));
+
+  let defs = svg.select("g#legend").append("defs");
+  let linearGradient = defs.append("linearGradient")
+      .attr("id", "linear-gradient")
+      .attr("gradientTransform", "rotate(90)");
+
+  linearGradient.selectAll("stop")
+    .data(color.ticks().map((t, i, n) => ({ offset: `${100*i/n.length}%`, color: color(max - t) })))
+    .enter().append("stop")
+    .attr("offset", d => d.offset)
+    .attr("stop-color", d => d.color);
+
+  svg.select("g#legend").append('g')
+    .attr("id", "legend-color")
+    .attr("transform", `translate(${width - config.margin.right - config.width}, ${config.margin.top})`)
+    .append("rect")
+    .attr('transform', `translate(0, ${config.margin.top})`)
+	.attr("height", config.height - config.margin.top)
+	.attr("width", config.width)
+	.style("fill", "url(#linear-gradient)");
+
+  svg.select("g#legend").append('g')
+    .call(legendAxis);
+}
 
 function convert(d) {
   d['value'] = +d['value'];
@@ -69,7 +252,13 @@ function convert(d) {
   return d;
 }
 
-function filterData(data) {
+function filterData(data, time) {
+  if (time) {
+    let parts = time.split('-');
+    data = data.filter(d => d['year'] == +parts[0] && d['month'] == +parts[1])
+    console.log(time);
+    console.log(data);
+  }
   let cluster = new Map();
 
   let mapping = {
@@ -108,39 +297,33 @@ function filterData(data) {
     });
   }
 
-  let clusterJson = []
-  // for v in cluster.values():
-  //     second = list()
-  //     for v2 in v['children'].values():
-  //         v2['children'] = list(v2['children'].values())
-  //         second.append(v2)
-  //     v['children'] = second
-  //     cluster_json.append(v)
+  let min = -1, max = -1;
+  let clusterJson = [];
   for (v of cluster.values()) {
     let second = [];
     for (v2 of v['children'].values()) {
-      v2['children'] = [...v2['children'].values()]
+      v2['children'] = [...v2['children'].values()];
+      for (elem of v2['children'].values()) {
+        if (min == -1 || min > elem.value) {
+          min = elem.value;
+        }
+        if (max == -1 || max < elem.value) {
+          max = elem.value;
+        }
+      }
       second.push(v2);
     }
     v['children'] = second;
     clusterJson.push(v);
   }
-  // cluster_json = {
-  //     'name': 'Fire Department',
-  //     'children': cluster_json
-  // }
   clusterJson = {
     'name': 'Fire Department',
     'children': clusterJson
-  }
-  return clusterJson;
-  // json.dumps(cluster_json, sort_keys=True)
-  // # cluster_json
+  };
+  return [clusterJson, min, max];
 }
 
 function autoBox() {
-  document.body.appendChild(this);
   const {x, y, width, height} = this.getBBox();
-  document.body.removeChild(this);
-  return [x, y, width, height];
+  return [x, y, width, height + 200];
 }
---
+++ assets/js/packing.js
@@ -0,0 +1,322 @@
+---
+---
+const diameter = 500;
+const width = 500 - 28 - 30;
+const height = 500 - 28 - 30;
+const pad = 14;
+const r = 5;
+
+const color = d3.scaleSequential(d3.interpolateYlOrRd);
+
+const depthColor = d3.scaleSequential([4, 0], d3.interpolateViridis);
+
+// let focus;
+// let rootFocus;
+// let view;
+// let node;
+
+const tip = d3.select("body")
+  .append("pre")
+  .attr("id", "tooltip")
+  .style("opacity", "0")
+  .style("background-color", "rgba(255, 255, 255, 0.7)")
+  .style("padding", "0px 5px")
+  .style("border-radius", "8px");
+
+d3.csv('{{ '/assets/data/dendrogram.csv' | prepend: site.baseurl }}', convert).then(data => {
+  console.log(data);
+  gData = data;
+  draw();
+  d3.select("g#packing")
+    .attr("viewBox", [0, 0, width, height])
+    .style("font", "10px sans-serif")
+    .attr("text-anchor", "middle");
+    // .on("click", () => zoom(rootFocus));
+
+  // zoomTo([rootFocus.x, rootFocus.y, rootFocus.r * 2]);
+});
+
+// function zoomTo(v) {
+//   // const k = width / v[2];
+//   const k = 1;
+//
+//   view = v;
+//
+//   node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+//   node.attr("r", d => d.r * k);
+// }
+
+// function zoom(d) {
+//   const focus0 = focus;
+//
+//   focus = d;
+//
+//   const transition = d3.select("svg#packing").transition()
+//       .duration(d3.event.altKey ? 7500 : 750)
+//       .tween("zoom", d => {
+//         const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+//         return t => zoomTo(i(t));
+//       });
+//
+//   // label
+//   //   .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+//   //   .transition(transition)
+//   //     .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+//   //     .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+//   //     .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+// }
+
+function drawNodes(g, nodes) {
+  console.log(nodes);
+  let circles = g.selectAll('circle')
+    .data(nodes, node => node.data.name)
+    .enter()
+    .append('circle')
+      .attr('r', d => d.r ? d.r : r)
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('id', d => garbageId(d))
+      .attr('class', 'node')
+      .attr('stroke', d => d.children? d3.lab(depthColor(d.height)).darker(): d3.lab(color(d.data.value)).darker())
+      .style('fill', d => d.children? depthColor(d.height): color(d.data.value));
+      // .on("click", d => focus !== d && (zoom(d), d3.event.stopPropagation()));
+  // node = circles;
+  setupEvents(g, circles);
+}
+
+function setupEvents(g, selection) {
+  selection.on('mouseover.highlight', function(d) {
+    let curr = d;
+    while (curr.parent) {
+      let baseId = garbageId(curr);
+      let me = d3.select(`circle#${baseId}`);
+      me.transition()
+        .attr("stroke", d.children? d3.lab(depthColor(d.height)).brighter(): d3.lab(color(d.data.value)).brighter())
+        .style("stroke-width", "1.5px");
+      curr = curr.parent;
+    }
+    d3.select(`circle#${garbageId(curr)}`).transition()
+      .attr("stroke", d.children? d3.lab(depthColor(d.height)).brighter(): d3.lab(color(d.data.value)).brighter())
+      .style("stroke-width", "1.5px");
+  });
+
+  selection.on('mouseout.highlight', function(d) {
+    let curr = d;
+    while (curr.parent) {
+      let baseId = garbageId(curr);
+      let me = d3.select(`circle#${baseId}`);
+      me.transition()
+        .attr("stroke", d.children? d3.lab(depthColor(d.height)).darker(): d3.lab(color(d.data.value)).darker())
+        .style("stroke-width", "1px");
+      curr = curr.parent;
+    }
+    d3.select(`circle#${garbageId(curr)}`).transition()
+      .attr("stroke", d.children? d3.lab(depthColor(d.height)).darker(): d3.lab(color(d.data.value)).darker())
+      .style("stroke-width", "1px");
+    // d3.select(this).transition()
+    //   .attr("stroke", d.children? d3.lab(depthColor(d.height)).darker(): d3.lab(color(d.data.value)).darker())
+    //   .style("stroke-width", "1px");
+  });
+
+  selection.on("mouseover.tooltip", function(d) {
+    tip.text(`${d.data.name}\n#: ${d3.format("~s")(d.value)}\nlevel: ${d.depth}`);
+    tip.transition().style("opacity", "1");
+  }).on("mousemove.tooltip", d => {
+    // get height of tooltip
+    let bbox = tip.node().getBoundingClientRect();
+
+    // https://stackoverflow.com/questions/4666367/how-do-i-position-a-div-relative-to-the-mouse-pointer-using-jquery
+    tip.style("left", (d3.event.pageX) + "px")
+    tip.style("top",  (d3.event.pageY - bbox.height) + "px");
+  }).on("mouseout.tooltip", function(d) {
+    tip.transition().style("opacity", "0");
+  });
+}
+
+function showTooltip(g, node) {
+  let d = node.datum;
+  // get height of tooltip
+  let bbox = tip.node().getBoundingClientRect();
+
+  // https://stackoverflow.com/questions/4666367/how-do-i-position-a-div-relative-to-the-mouse-pointer-using-jquery
+  tip.style("left", (d3.event.pageX) + "px")
+  tip.style("top",  (d3.event.pageY - bbox.height) + "px");
+  tip.text(`${d.name}\n#: ${d3.format("~s")(d.value)}`);
+  tip.transition().style("opacity", "1");
+}
+
+function draw(time) {
+  let data = gData;
+  let [clusterJson, min, max] = time? filterData(data, time): filterData(data);
+  color.domain([min, max]);
+  data = clusterJson;
+
+  let pack = data => d3.pack()
+    .size([width - 2, height - 2])
+    .padding(3)
+  (d3.hierarchy(data)
+    .sum(d => d.value)
+    .sort((a, b) => b.value - a.value))
+
+  data = pack(data);
+  // focus = data;
+  // rootFocus = data;
+
+  let svg = d3.select("svg#packing")
+      .style("width", diameter)
+      .style("height", diameter);
+
+  let plot = svg.select("g#plot")
+    .attr("transform", `translate(${pad}, ${pad})`);
+
+  drawNodes(plot.append("g"), data.descendants());
+  drawLegend(min, max);
+}
+
+function garbageId(d) {
+  let id = d.parent?
+    d.parent.parent?
+      d.parent.parent.parent?
+        `${d.parent.parent.parent.data.name}_${d.parent.parent.data.name}_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+        :
+        `${d.parent.parent.data.name}_${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+      :
+      `${d.parent.data.name}_${d.data.name}`.replace(/\s/g,'')
+    :
+    `${d.data.name}`.replace(/\s/g,'');
+  return id;
+}
+
+function drawLegend(min, max) {
+  let width = diameter + 20;
+  let config = {
+    "margin": {
+      "left": 50,
+      "right": 50,
+      "top": 50,
+      "bot": 50
+    },
+    "height": height - 200,
+    "width": 8
+  }
+  // Sequential Legend created from following Tom MacWright's example
+  // here: https://observablehq.com/@tmcw/d3-scalesequential-continuous-color-legend-example
+  d3.select("svg#packing").select("g#legend").append("text")
+    .attr("x", width - config.margin.right - 8)
+    .attr("y", 2*config.margin.top - 5)
+    .text("#");
+
+  let legendScale = d3.scaleLinear()
+    .domain(color.domain().reverse())
+    .range([config.margin.top, config.height])
+
+  let legendAxis = g => g
+    .attr("class", `legend-axis`)
+    .attr("transform", `translate(${width - config.margin.right}, ${config.margin.top})`)
+    .attr("height", config.height)
+    .call(d3.axisRight(legendScale)
+      .tickFormat(d3.format("~s"))
+      .ticks(config.height / 40)
+      .tickSize(-config.width));
+
+  let defs = d3.select("svg#packing").select("g#legend").append("defs");
+  let linearGradient = defs.append("linearGradient")
+      .attr("id", "linear-gradient")
+      .attr("gradientTransform", "rotate(90)");
+
+  linearGradient.selectAll("stop")
+    .data(color.ticks().map((t, i, n) => ({ offset: `${100*i/n.length}%`, color: color(max - t) })))
+    .enter().append("stop")
+    .attr("offset", d => d.offset)
+    .attr("stop-color", d => d.color);
+
+  d3.select("svg#packing").select("g#legend").append('g')
+    .attr("id", "legend-color")
+    .attr("transform", `translate(${width - config.margin.right - config.width}, ${config.margin.top})`)
+    .append("rect")
+    .attr('transform', `translate(0, ${config.margin.top})`)
+	.attr("height", config.height - config.margin.top)
+	.attr("width", config.width)
+	.style("fill", "url(#linear-gradient)");
+
+  d3.select("svg#packing").select("g#legend").append('g')
+    .call(legendAxis);
+}
+
+function convert(d) {
+  d['value'] = +d['value'];
+  d['month'] = +d['month'];
+  d['year'] = +d['year'];
+  return d;
+}
+
+function filterData(data, time) {
+  if (time) {
+    let parts = time.split('-');
+    data = data.filter(d => d['year'] == +parts[0] && d['month'] == +parts[1])
+    console.log(time);
+    console.log(data);
+  }
+  let cluster = new Map();
+
+  let mapping = {
+    'B01': 'D02',
+    'B04': 'D02',
+    'B05': 'D02',
+    'B07': 'D02',
+    'B08': 'D02',
+    'B02': 'D03',
+    'B03': 'D03',
+    'B06': 'D03',
+    'B09': 'D03',
+    'B10': 'D03'
+  };
+
+  for (row of data) {
+    if (!cluster.has(row['Division'])) {
+      cluster.set(row['Division'], {
+        'name': row['Division'],
+        'children': new Map()
+      });
+    }
+    if (!cluster.get(row['Division'])['children'].has(row['Battalion'])) {
+      cluster.get(row['Division'])['children'].set(row['Battalion'], {
+        'name': row['Battalion'],
+        'children': new Map()
+      });
+    }
+    let value = 0;
+    if (cluster.get(row['Division'])['children'].get(row['Battalion'])['children'].has(row['Station Area'])) {
+      value = cluster.get(row['Division'])['children'].get(row['Battalion'])['children'].get(row['Station Area'])['value'];
+    }
+    cluster.get(row['Division'])['children'].get(row['Battalion'])['children'].set(row['Station Area'], {
+      'name': row['Station Area'],
+      'value': value + row['value']
+    });
+  }
+
+  let min = -1, max = -1;
+  let clusterJson = [];
+  for (v of cluster.values()) {
+    let second = [];
+    for (v2 of v['children'].values()) {
+      v2['children'] = [...v2['children'].values()];
+      for (elem of v2['children'].values()) {
+        if (min == -1 || min > elem.value) {
+          min = elem.value;
+        }
+        if (max == -1 || max < elem.value) {
+          max = elem.value;
+        }
+      }
+      second.push(v2);
+    }
+    v['children'] = second;
+    clusterJson.push(v);
+  }
+  clusterJson = {
+    'name': 'Fire Department',
+    'children': clusterJson
+  };
+  return [clusterJson, min, max];
+}
